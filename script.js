2f788e1221be3fbd09d15b729317dd2949f73be8
Dennis Zuniga <d.zun93@gmail.com>
2025-04-11 21:00:02 -0600

    No changes made here

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Add interactivity to the button
 document.getElementById('clickMe').addEventListener('click', function () {
     const message = document.getElementById('message');
-    message.textContent = 'You clicked the button! Great job!';
+    message.textContent = 'Te re-super amo mucho bella hermosa preciosa cosita sexy que me encanta! <3';
     message.style.color = '#0078d7';
 });
